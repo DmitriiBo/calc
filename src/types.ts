15c7fb34dad428f4f80f6c5ae81726a9b2917392
DmitriiBo <alienshooter2008@yandex.ru>
2021-02-28 18:04:18 +0300

    Initial dist subtree commit

--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export type ExistsKeys = "+" | "-" | "=" | "C" | number;
-export type AllowedSymbol = "+" | "-" | number;
-export type IntermediateOperator = '+' | '-';
-export type CurrentInputSymbols = AllowedSymbol[];
